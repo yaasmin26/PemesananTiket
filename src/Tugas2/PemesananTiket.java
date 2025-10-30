434fe2561dbd7d32b719728dbe1efc69d3756cfa
yaasmin26 <yaasminramadhani@gmail.com>
2025-10-30 22:53:36 +0700

    Initial commit - Program Pemesanan Tiket dengan JavaDoc

--- src/Tugas2/PemesananTiket.java
+++ src/Tugas2/PemesananTiket.java
@@ -9,95 +9,103 @@ package Tugas2;
 
 public class PemesananTiket {
     /**
-     * Diskon tetap sebesar 10%*/
+     * Diskon tetap sebesar 10%
+     */
     public static final double DISKON = 0.9;
     /**
-     * Judul film yang dipesan*/
+     * Judul film yang dipesan
+     */
     private String judulFilm;
     /**
-     * Jumlah tiket yang dipesan*/
+     * Jumlah tiket yang dipesan
+     */
     private int jumlahTiket;
     /**
-     * Harga per tiket*/
+     * Harga per tiket
+     */
     private double hargaTiket;
+    private boolean member;
 
     /**
      *
      * @return judul film
      */
     public String getJudulFilm() {
-            return judulFilm;
-        }
+        return judulFilm;
+    }
 
     /**
      *
      * @param judulFilm judul film yang akan ditonton
      */
     public void setJudulFilm(String judulFilm) {
-            this.judulFilm = judulFilm;
-        }
+        this.judulFilm = judulFilm;
+    }
 
     /**
      *
      * @return jumlah tiket
      */
     public int getJumlahTiket() {
-            return jumlahTiket;
-        }
+        return jumlahTiket;
+    }
 
     /**
      *
      * @param jumlahTiket jumlah tiket yang dipesan
      */
     public void setJumlahTiket(int jumlahTiket) {
-            this.jumlahTiket = jumlahTiket;
-        }
+        this.jumlahTiket = jumlahTiket;
+    }
 
     /**
      *
      * @return harga tiket
      */
     public double getHargaTiket() {
-            return hargaTiket;
-        }
+        return hargaTiket;
+    }
 
     /**
      *
-     * @param hargaTiket harga satu tiket     */
+     * @param hargaTiket harga satu tiket
+     */
     public void setHargaTiket(double hargaTiket) {
-            this.hargaTiket = hargaTiket;
-        }
+        this.hargaTiket = hargaTiket;
+    }
 
     /**
      * Konstruktor untuk membuat objek PemesananTiket baru.
+     *
      * @param judulFilm
      * @param jumlahTiket
      * @param hargaTiket
      */
 
-        public PemesananTiket(String judulFilm, int jumlahTiket, double hargaTiket) {
-            this.setJudulFilm(judulFilm);
-            this.setJumlahTiket(jumlahTiket);
-            this.setHargaTiket(hargaTiket);
-        }
+    public PemesananTiket(String judulFilm, int jumlahTiket, double hargaTiket) {
+        this.setJudulFilm(judulFilm);
+        this.setJumlahTiket(jumlahTiket);
+        this.setHargaTiket(hargaTiket);
+    }
 
     /**
      * Menampilkan detail tiket beserta total harga setelah diskon.
-      */
-    private boolean member = true;
+     */
     public void tampilkanDetailTiket() {
-            System.out.println("Judul Film: " + getJudulFilm());
-            System.out.println("Jumlah Tiket: " + getJumlahTiket());
-            System.out.println("Harga per Tiket: " + getHargaTiket());
-            System.out.println("Total Harga (dengan diskon 10%): " + hitungTotalHarga());
-        }
+        System.out.println("Judul Film: " + getJudulFilm());
+        System.out.println("Jumlah Tiket: " + getJumlahTiket());
+        System.out.println("Harga per Tiket: " + getHargaTiket());
+        System.out.println("Total Harga (dengan diskon 10%): " + hitungTotalHarga());
+    }
 
     /**
      * Menghitung total harga tiket setelah diskon.
+     *
      * @return total harga setelah diskon
      */
     private double hitungTotalHarga() {
-            return getJumlahTiket() * getHargaTiket() *DISKON;
-        }
+        double total = getJumlahTiket() * getHargaTiket() * DISKON;
+        if (member) total *= 0.95; // diskon tambahan 5% untuk member
+        return total;
     }
-
+}
